d74e980e8abadd8cc04b27ac084154e9ae1955de
Jami Äijänen <jami.aijanen@hotmail.com>
2022-02-13 14:55:05 +0200

    first commit

--- App.js
+++ App.js
@@ -1,10 +1,33 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Alert, StyleSheet, Text, View, Image } from 'react-native';
+import React from 'react';
+import { useState } from 'react';
+import { Button, FlatList, TextInput, StatusBar } from 'react-native';
 
 export default function App() {
+  const [hakusana, setHakusana] = useState('');
+  const [repo, setRepo] = useState([]);
+
+  const getRepo = () => {
+    fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${hakusana}`)
+    .then(res => res.json())
+    .then(data => setRepo(data.meals))
+    .catch(error => {
+      Alert.alert('Error', error);
+    });
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <FlatList 
+        keyExtractor={(item, index) => index.toString()}
+        renderItem={({item}) => 
+          <View>
+            <Text>{item.strMeal}</Text>
+            <Image source = {{uri: item.strMealThumb}} style = {{ width: 200, height: 200 }} />
+          </View>}
+          data={repo} />
+      <TextInput placeholder= 'keyword' onChangeText={ text => setHakusana(text) } />
+      <Button title= "Find" onPress= {getRepo} />
       <StatusBar style="auto" />
     </View>
   );
